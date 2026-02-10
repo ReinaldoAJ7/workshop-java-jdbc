3de655d1396addd441acc06c33dfdf5c5da37484
ReinaldoAJ7 <142172046+ReinaldoAJ7@users.noreply.github.com>
2026-02-10 09:50:05 -0300

    About View created.

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 
@@ -30,12 +39,29 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemAboutAction() {
-		System.out.println("Dreams About");
+		loadView("/gui/About.fxml");
 	}
 	
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
 	}
 	
+	private synchronized void loadView(String absoluteName) {
+		try {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+		VBox newVBox = loader.load();
+		
+		Scene mainScene = Main.getMainScene();
+		VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+		
+		Node mainMenu = mainVBox.getChildren().get(0);//get the first element of the main VBox, which is the menu
+		mainVBox.getChildren().clear();//clear all the content of the main VBox
+		mainVBox.getChildren().add(mainMenu);//add the menu back to the main VBox
+		mainVBox.getChildren().addAll(newVBox.getChildren());//add the new content of the new VBox to the main VBox
+		}
+		catch(IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
 
 }
