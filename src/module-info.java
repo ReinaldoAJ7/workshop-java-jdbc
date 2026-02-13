9925984cc9c019bb03e32c1920b7ba1d9b46142d
ReinaldoAJ7 <142172046+ReinaldoAJ7@users.noreply.github.com>
2026-02-12 22:28:05 -0300

    Adding database access

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module workshop_java_jdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires java.sql;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.fxml;
